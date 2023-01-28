aaeed34a8b7816e64c6d87a661ca437c9682cd15
danilduborezov <ygra.pl@gmail.com>
2023-01-28 18:04:42 +0300

    change brain_calc script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games import brain_calc_logic
+from brain_games.brain_engine import playing
+
+
+def main():
+    playing(brain_calc_logic)
+
+
+if __name__ == '__main__':
+    main()
