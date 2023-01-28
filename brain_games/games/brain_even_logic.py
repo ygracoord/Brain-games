691f51baa4233b67e4b490061b1ccf48f1b76920
danilduborezov <ygra.pl@gmail.com>
2023-01-28 19:23:57 +0300

    refactor module

--- brain_games/games/brain_even_logic.py
+++ brain_games/games/brain_even_logic.py
@@ -1,13 +1,15 @@
 import random
+import prompt
 
 TITLE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def entry_data():
     random_num = random.randint(1, 100)
-    user_answer = random_num
+    user_answer = prompt.secret(f'Question: {random_num} ')
+
     if random_num % 2 == 0:
         success_answer = 'yes'
     else:
         success_answer = 'no'
-    return user_answer, success_answer
+    return user_answer, str(success_answer)
