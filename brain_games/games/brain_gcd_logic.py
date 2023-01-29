809ad0196bec906ab82a4cfd8a287eb67b66d760
danilduborezov <ygra.pl@gmail.com>
2023-01-29 11:34:36 +0300

    create new game gcd

---
+++ brain_games/games/brain_gcd_logic.py
@@ -0,0 +1,12 @@
+import math
+import prompt
+import random
+
+TITLE = 'Find the greatest common divisor of given numbers.'
+
+
+def entry_data():
+    number1, number2 = random.randint(1, 50), random.randint(1, 50)
+    user_answer = prompt.secret(f'Question: {number1} {number2} ')
+    success_answer = math.gcd(number1, number2)
+    return user_answer, str(success_answer)
