905b6aef015e575f9acb815236fffa6af038afc8
danilduborezov <ygra.pl@gmail.com>
2023-01-29 19:32:08 +0300

    create brain-progress logic

---
+++ brain_games/games/brain_progression_logic.py
@@ -0,0 +1,21 @@
+import random
+import prompt
+
+TITLE = 'What number is missing in the progression?'
+
+
+def entry_data():
+    progression_list = []
+    start, finish = random.randint(1, 10), random.randint(100, 110)
+    random_step = random.randint(1, 5)
+
+    for i in range(start, finish, random_step):
+        progression_list.append(str(i))
+
+        if len(progression_list) == 10:
+            missed_elem = random.randint(0, len(progression_list) - 1)
+            success_answer = str(progression_list[missed_elem])
+            progression_list[missed_elem] = '..'
+            final_list = ' '.join(progression_list[:10])
+            user_answer = prompt.secret(f'Question: {final_list} ')
+            return user_answer, success_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.brain_engine import playing
+from brain_games.games import brain_progression_logic
+
+
+def main():
+    playing(brain_progression_logic)
+
+
+if __name__ == '__main__':
+    main()
