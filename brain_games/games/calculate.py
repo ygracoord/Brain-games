ccc330406c4fc1245e2ae219436c12dfbe61f898
danilduborezov <ygra.pl@gmail.com>
2023-02-09 11:23:44 +0300

    Delete f-string

--- brain_games/games/calculate.py
+++ brain_games/games/calculate.py
@@ -8,7 +8,7 @@ def get_question_and_answer():
     number1, number2 = random.randint(1, 30), random.randint(1, 30)
     operator = random.choice(['+', '-', '*'])
     print(f'Question: {number1} {operator} {number2}')
-    user_answer = prompt.string(f'Your answer: ')
+    user_answer = prompt.string('Your answer: ')
     success_answer = calculate_result(number1, number2, operator)
     return user_answer, str(success_answer)
 
