39ddb673c5e5c12815bcdec9fed2738dfa4c7ecf
danilduborezov <ygra.pl@gmail.com>
2023-02-09 10:40:13 +0300

    Rename module

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.games import brain_even_logic
+from brain_games.games import even
 from brain_games.engine import run_game
 
 
 def main():
-    run_game(brain_even_logic)
+    run_game(even)
 
 
 if __name__ == '__main__':
