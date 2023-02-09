6a63c80ce5beab2b74e0f513d0a93be180e81f22
danilduborezov <ygra.pl@gmail.com>
2023-02-09 10:10:53 +0300

    Rename to engine.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from brain_games.games import brain_even_logic
-from brain_games.brain_engine import run_game
+from brain_games.engine import run_game
 
 
 def main():
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from brain_games.brain_engine import run_game
+from brain_games.engine import run_game
 from brain_games.games import brain_gcd_logic
 
 
