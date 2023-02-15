69d232a3a04a60e0dab1a8057412fddbb8d96521
danilduborezov <ygra.pl@gmail.com>
2023-02-15 18:42:16 +0300

    Change logic add question in general

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,8 +10,9 @@ def run_game(game):
     print(game.TITLE)
 
     for _ in range(ATTEMPTS):
-        user_answer, success_answer = game.get_question_and_answer()
-
+        question, success_answer = game.get_question_and_answer()
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
         if user_answer != success_answer:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{success_answer}'.")
