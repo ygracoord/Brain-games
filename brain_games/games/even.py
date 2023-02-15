1463d975cdd180fd6610f5c7208dc704bfe45674
danilduborezov <ygra.pl@gmail.com>
2023-02-15 20:02:14 +0300

    Rolled into a ternary operator

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,16 +1,10 @@
 import random
-import prompt
 
 TITLE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_and_answer():
     random_num = random.randint(1, 100)
-    print(f'Question: {random_num}')
-    user_answer = prompt.string('Your answer: ')
-
-    if random_num % 2 == 0:
-        success_answer = 'yes'
-    else:
-        success_answer = 'no'
-    return user_answer, str(success_answer)
+    question = f'{random_num}'
+    success_answer = 'yes' if random_num % 2 == 0 else 'no'
+    return question, success_answer
