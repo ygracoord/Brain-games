1580b602d8b077c7b6f33c21a0461c3c5865ebfa
danilduborezov <ygra.pl@gmail.com>
2023-02-16 12:38:21 +0300

    Add 0 and 1 to condition

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,6 +4,8 @@ TITLE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
+    if num == 0 or abs(num) == 1:
+        return False
     for i in range(2, int(abs(num) / 2) + 1):
         if abs(num) % i == 0:
             return False
