fe6434c4b212b3cc33a2b49c175fd2bebd568aac
danilduborezov <ygra.pl@gmail.com>
2023-02-16 13:38:07 +0300

    Rewrite condition

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,10 +4,10 @@ TITLE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    if num == 0 or abs(num) == 1:
+    if num < 1:
         return False
-    for i in range(2, int(abs(num) / 2) + 1):
-        if abs(num) % i == 0:
+    for i in range(2, int(num / 2) + 1):
+        if num % i == 0:
             return False
     return True
 
