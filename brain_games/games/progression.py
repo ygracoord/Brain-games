3890cb7aba3a7a0297f2cb57bf5e1b3c82561581
danilduborezov <ygra.pl@gmail.com>
2023-02-16 10:48:28 +0300

    Completely change the structure of the code

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,22 +1,16 @@
 import random
-import prompt
 
 TITLE = 'What number is missing in the progression?'
 
 
 def get_question_and_answer():
-    progression_list = []
-    start, finish = random.randint(1, 10), random.randint(100, 110)
-    random_step = random.randint(1, 5)
-
-    for i in range(start, finish, random_step):
-        progression_list.append(str(i))
-
-        if len(progression_list) == 10:
-            missed_elem = random.randint(0, len(progression_list) - 1)
-            success_answer = str(progression_list[missed_elem])
-            progression_list[missed_elem] = '..'
-            final_progression = ' '.join(progression_list[:10])
-            print(f'Question: {final_progression}')
-            user_answer = prompt.string('Your answer: ')
-            return user_answer, success_answer
+    start = random.randint(1, 10)
+    step = random.randint(1, 5)
+    finish = (start + 10 * step)
+    progression = list(map(str, range(start, finish, step)))
+    missed_elem = random.randint(0, len(progression) - 1)
+    success_answer = str(progression[missed_elem])
+    progression[missed_elem] = '..'
+    final_progression = ' '.join(progression)
+    question = f'{final_progression}'
+    return question, success_answer
