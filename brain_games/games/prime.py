2e39080e1322ddb39b86fd1a8d68bd0a31b588ff
danilduborezov <ygra.pl@gmail.com>
2023-02-16 12:19:45 +0300

    Change structure of code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,8 +4,8 @@ TITLE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    for i in range(2, (num // 2) + 1):
-        if num % i == 0:
+    for i in range(2, int(abs(num) / 2) + 1):
+        if abs(num) % i == 0:
             return False
     return True
 
@@ -14,4 +14,4 @@ def get_question_and_answer():
     random_num = random.randint(-100, 100)
     question = random_num
     success_answer = 'yes' if is_prime(random_num) else 'no'
-    return question, str(success_answer)
+    return question, success_answer
