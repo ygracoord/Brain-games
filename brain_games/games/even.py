1f82673631a0aceb6fb89a34f2eb15dc1fb2a6e2
danilduborezov <ygra.pl@gmail.com>
2023-02-16 12:57:13 +0300

    Rewrite question

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,7 +4,6 @@ TITLE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_and_answer():
-    random_num = random.randint(1, 100)
-    question = f'{random_num}'
-    success_answer = 'yes' if random_num % 2 == 0 else 'no'
+    question = random.randint(1, 100)
+    success_answer = 'yes' if question % 2 == 0 else 'no'
     return question, success_answer
