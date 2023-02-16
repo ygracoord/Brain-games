135c2d497117de5ee74b58cf4654a72bf13f2b92
danilduborezov <ygra.pl@gmail.com>
2023-02-16 10:05:45 +0300

    Completely change the structure of the code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,17 +5,14 @@ TITLE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    d = 2
-    while d * d <= num and num % d != 0:
-        d += 1
-        if d * d > num:
-            return 'yes'
-    return 'no'
+    for i in range(2, (num // 2) + 1):
+        if num % 2 == 0:
+            return False
+        return True
 
 
 def get_question_and_answer():
-    random_num = random.randint(2, 100)
-    print(f'Question: {random_num}')
-    user_answer = prompt.string('Your answer: ')
-    success_answer = is_prime(random_num)
-    return user_answer, str(success_answer)
+    random_num = random.randint(-100, 100)
+    question = f'{random_num}'
+    success_answer = 'yes' if is_prime(random_num) else 'no'
+    return question, success_answer
