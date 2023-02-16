e2bc1c30e98f158c3e9f6fff5fdb3fb585e229c7
danilduborezov <ygra.pl@gmail.com>
2023-02-16 10:12:33 +0300

    Change structure of code

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,5 +1,4 @@
 import math
-import prompt
 import random
 
 TITLE = 'Find the greatest common divisor of given numbers.'
@@ -7,7 +6,6 @@ TITLE = 'Find the greatest common divisor of given numbers.'
 
 def get_question_and_answer():
     number1, number2 = random.randint(1, 50), random.randint(1, 50)
-    print(f'Question: {number1} {number2}')
-    user_answer = prompt.string('Your answer: ')
+    question = f'{number1} {number2}'
     success_answer = math.gcd(number1, number2)
-    return user_answer, str(success_answer)
+    return question, str(success_answer)
